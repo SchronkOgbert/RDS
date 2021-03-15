8fe2a30e71fa59996fa5f6b4580dffd2a1c5a67f
Horia <horia.spataru01@e-uvt.ro>
2021-03-15 22:04:37 +0200

    Am creeat meniu

--- RDS-code/main.c
+++ RDS-code/main.c
@@ -1,13 +1,68 @@
 #include "core.h"
 
+int option;
+
+void startmsg();
+void showmenu();
+
 int main()
 {
 	setup(); //this function needs to be the first line to be executed
-	
-	
+
+
+	startmsg();
+
+	while (option < 5)
+	{
+		switch (option)
+		{
+		case 1:
+		{
+			//aici vine functia
+			showmenu();
+			break;
+		}
+		case 2:
+		{
+			//aici vine functia
+			break;
+		}
+		case 3:
+		{
+			//aici vine functia
+			break;
+		}
+		case 4:
+		{
+			//aici vine functia
+			break;
+		}
+
+		}
+	}
+
 	prepare_quit(); //this function needs to be last to be executed
 
 	//system("pause");
 
 	return 0;
+}
+
+void startmsg()
+{
+	printf("Bine ati venit la TmRDS!\n");
+	showmenu();
+}
+
+void showmenu()
+{
+	printf("Va rugam alegeti optiunea:\n");
+	printf("1.Cautati suma de plata dupa cnp.\n");
+	printf("2.Afisati abonamentele telefonice.\n");
+	printf("3.Afisati numarul de abonati care au abonament la cel putin 3 numere de telefon.\n");
+	printf("4.Creeaza un contract.\n");
+	printf("5.Inchideti aplicatia.\n");
+	printf("Introduceti numarul optiunii: ");
+	scanf("%d", &option);
+	printf("\n");
 }

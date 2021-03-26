c9b93f34cd625d2d2168bcf95e7ba50e4df120b9
Schronk <damian.sirbu01@e-uvt>
2021-03-26 17:29:29 +0200

    forgot to delete a call

--- RDS-code/main.c
+++ RDS-code/main.c
@@ -5,7 +5,6 @@ int main()
 	setup(); //this function needs to be the first line to be executed
 	
 	printf("%s", search_bills(123456));
-	set_bill_data(search_bills(123456));
 
 	prepare_quit(); //this function needs to be last to be executed
 
